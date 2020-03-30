7773cb973ec4884e5a2b55c1ad6a8a62daa1f62c
Leopold Talirz <leopold.talirz@gmail.com>
2020-03-30 02:13:54 +0200

    try fixing docs

--- aiida_testing/mock_code/__init__.py
+++ aiida_testing/mock_code/__init__.py
@@ -4,4 +4,12 @@ Defines fixtures for mocking AiiDA codes, with caching at the level of
 the executable.
 """
 
-from ._fixtures import *  #  NOQA
+from ._fixtures import *
+
+__all__ = (
+    "pytest_addoption",
+    "testing_config_action",
+    "mock_regenerate_test_data",
+    "testing_config",
+    "mock_code_factory",
+)
